7a572b076f20e7c998863043e686b49e94533347
Emmanuel Anebi <anebiemmanuel@gmail.com>
2022-05-03 16:58:04 +0100

    feat: initial landing page setup

--- nftbook/urls.py
+++ nftbook/urls.py
@@ -15,16 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("nftbook.apps.nfts.urls"))
+    path("", include("nftbook.apps.accounts.urls")),
+    path("", include("nftbook.apps.nfts.urls")),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
+        path('__debug__/', include(debug_toolbar.urls))
     ]
+    # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
